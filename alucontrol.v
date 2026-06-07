d908112d0a7fdba28f3b638c6123c9d8f923fc1a
Massa Hassoun <massa.hassoun@ogr.iuc.edu.tr>
2026-06-07 14:08:51 +0300

    Refactor alucontrol module for improved clarity

--- alucontrol.v
+++ alucontrol.v
@@ -1,21 +1,23 @@
-module alucontrol(AluOp,FnField,AluCtrl);
+module alucontrol(AluOp, FnField, AluCtrl);
 
 input [1:0] AluOp;
-input [5:0] FnField; /for R-type instruction
+input [5:0] FnField;
 
 output reg [3:0] AluCtrl;
 
-
-always@(AluOp or FnField)begin
-	casex({AluOp,FnField})
-		8'b00_xxxxxx:AluCtrl=4'b0010; //lw / sw
-		8'b01_xxxxxx:AluCtrl=4'b0110; //beq
-		8'b1x_xx0000:AluCtrl=4'b0010; //add
-		8'b1x_xx0010:AluCtrl=4'b0110; //sub
-		8'b1x_xx0100:AluCtrl=4'b0000; //and
-		8'b1x_xx0101:AluCtrl=4'b0001; //or
-		8'b1x_xx1010:AluCtrl=4'b0111; //slt
-	endcase
+always @(AluOp or FnField) begin
+    casex({AluOp, FnField})
+        8'b11_xxxxxx: AluCtrl = 4'b1101;
+        8'b00_xxxxxx: AluCtrl = 4'b0010;
+        8'b01_xxxxxx: AluCtrl = 4'b0110;
+        8'b1x_xx0000: AluCtrl = 4'b0010;
+        8'b1x_xx0010: AluCtrl = 4'b0110;
+        8'b1x_xx0100: AluCtrl = 4'b0000;
+        8'b1x_xx0101: AluCtrl = 4'b0001;
+        8'b1x_xx1010: AluCtrl = 4'b0111;
+        8'b1x_xx1100: AluCtrl = 4'b1110;
+        default:      AluCtrl = 4'b0010;
+    endcase
 end
 
 endmodule
